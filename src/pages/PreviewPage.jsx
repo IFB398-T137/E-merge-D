eff5d31d7f7c53d1688f8efff293da7d6ecb74f3
ptrac2 <patrick.tracey7@gmail.com>
2026-05-01 12:38:38 +1000

    Set up React app with upload template and preview page flow

---
+++ src/pages/PreviewPage.jsx
@@ -0,0 +1,10 @@
+function PreviewPage({ onBack }) {
+  return (
+    <div>
+      <h1>Preview Page</h1>
+      <button onClick={onBack}>Back</button>
+    </div>
+  );
+}
+
+export default PreviewPage;
---
+++ src/pages/UploadPage.jsx
@@ -0,0 +1,10 @@
+function UploadPage({ onNext }) {
+  return (
+    <div>
+      <h1>Upload Page</h1>
+      <button onClick={onNext}>Next</button>
+    </div>
+  );
+}
+
+export default UploadPage;
