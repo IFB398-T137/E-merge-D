1ab882c74ef0c7eef05a89fd02639ced9f82dcf2
Liam Clarke <liamclarke2010@hotmail.com>
2026-05-11 22:30:24 +1000

    Working electron launched throug vite with local host all done through vs code -No not write over this as it will be used for rollbacks

---
+++ electron/main.cjs
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  const isDev = !app.isPackaged;
+
+  if (isDev) {
+    // Vite dev server
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    // PRODUCTION build (IMPORTANT FIX)
+    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
